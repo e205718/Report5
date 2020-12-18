6eb67b35008e0c5677516374d688fcb75ca52e10
e205718 <e205718@ie.u-ryukyu.ac.jp>
2020-12-19 02:49:03 +0900

    step2

--- EroorEx.java
+++ EroorEx.java
@@ -2,6 +2,13 @@ public class EroorEx {
     static String str = null;
 
     public static void main(String[] args) {
+
+
+        try{
         System.out.println(str.length());
+        }catch(NullPointerException e){
+            System.out.println("NULLが入力されています");
+            System.out.println(e.getMessage());
+        }
     }
 }
